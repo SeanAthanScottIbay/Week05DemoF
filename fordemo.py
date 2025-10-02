effabcaf2332dd9a656d5737ad0ceb74eca923fc
Sean Athan Ibay <Sean Athan Ibay@SEYAN>
2025-10-02 13:37:37 +0800

    Added demo files

---
+++ fordemo.py
@@ -0,0 +1,10 @@
+# for demo
+
+num = int(input("Enter a number: "))
+
+#for i in range(num):       # default; starts with 0
+#for i in range(1, num+1):  # start with 1
+for i in range(0, num, 2):  # skips every 2
+    print(i)
+
+print("Thank you for using this program.")
